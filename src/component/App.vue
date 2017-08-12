5bff038ba17b55df3d816cfd59cbd996afb84ddc
xiaohai <2282403830@qq.com>
2017-08-12 22:15:22 +0800

    vue项目脚手架搭建

---
+++ src/component/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-header></v-header>
+    <router-view></router-view>
+    <v-footer></v-footer>
+</template>
+
+<script>
+import Cheader from './home/header.vue';
+import Cfooter from './home/footer.vue';
+export default {
+    components: {
+        'v-header': Cheader,
+        'v-footer': Cfooter
+    }
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/component/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <p>这是APP的首页</p>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    
+</style>
